3d2197e9ab2b392145e1522fe9b2652951a0289f
codeNow26 <code.now26@gmail.com>
2025-07-28 11:04:35 +0200

    css for the drop-down-menu, inserted badge and checkbox

--- add_task.js
+++ add_task.js
@@ -2,14 +2,64 @@ let urgentActive = false;
 let mediumActive = false;
 let lowActive = false;
 let task = [];
-let Users = [
-    "Anja", "Peter", "Ralf", "Lisa", "Raphael", "Paul",
-]
-
+let names = [];
 
 function init() {
     removeRequired();
+    getAllUser("/users")
 }
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDaAKocqkIROo_InISQbRjsoG8z1JCK3g0",
+    authDomain: "join-gruppenarbeit-75ecf.firebaseapp.com",
+    databaseURL: "https://join-gruppenarbeit-75ecf-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "join-gruppenarbeit-75ecf",
+    storageBucket: "join-gruppenarbeit-75ecf.firebasestorage.app",
+    messagingSenderId: "180158531840",
+    appId: "1:180158531840:web:c894124a7d6eb515364be5",
+    measurementId: "G-5R563MH52P"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+const FIREBASE_URL = "https://join-gruppenarbeit-75ecf-default-rtdb.europe-west1.firebasedatabase.app/"
+
+
+async function getAllUser(path = "") {
+    let response = await fetch(FIREBASE_URL + path + ".json");
+    let responseToJson = await response.json();
+    let users = Object.values(responseToJson);
+
+    for (let i = 1; i < users.length; i++) {
+    }
+    return users;
+}
+
+// Es werden alle Userdaten aus der Datenbank geholt und als return zurückgegeben.
+
+async function showUserName() {
+    let dropList = document.getElementById('dropdownList');
+    let users = await getAllUser("/users");
+    dropList.innerHTML = "";
+
+    for (let i = 1; i < users.length; i++) {
+        let div = document.createElement("div");
+        let name = document.createElement("span");
+        let img = document.createElement("img");
+        div.classList.add("Assigned-dropdown-username");
+        name.textContent = users[i].name;
+        img.src = users[i].badge;
+
+        div.appendChild(img);
+        div.appendChild(name);
+        div.appendChild(CheckButton());
+        dropList.appendChild(div);
+    }
+}
+
+// Es wird pro Name in der Datenbank eine div mit dem Namen und dem Badge generiert und der Checkbutton eingefügt.
 
 function resetAllButton() {
     document.getElementById('urgent').classList.remove('bg-red');
@@ -139,16 +189,26 @@ function inputBorderColorSwitch() {
     assignInput.classList.toggle("borderColorBlue");
 }
 
-function renderCategoryDropdown() {
+function renderAssingDropdown() {
     let arrowIcon = document.getElementById('drop-down-svg');
     let currentSrc = arrowIcon.src;
     let dropdownList = document.getElementById('dropdownList');
 
     if (currentSrc.includes("arrow_drop_down.svg")) {
         arrowIcon.src = "./assets/arrow_drop_down2.svg";
-        dropdownList.classList.remove("hidden");
+        dropdownList.classList.toggle('open');
     } else {
         arrowIcon.src = "./assets/arrow_drop_down.svg";
-        dropdownList.classList.add("hidden");
+        dropdownList.classList.remove('open');
     }
 }
+
+// Das Assigned to Input wird gerendert und befüllt mit den User Daten aus der Firebase Datenbank.
+
+function CheckButton() {
+   let checkButton = document.createElement("img")
+   checkButton.classList.add("check-button");
+   checkButton.src = "./assets/Check button.svg";
+   return checkButton;
+}
+
