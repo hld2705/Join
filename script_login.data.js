3b46ea133075150c0ac476bb4afb111067bfe758
Halid Crnkic <halidcrnkic@gmail.com>
2025-07-30 17:10:48 +0200

    user login functions, animation, responsive

---
+++ script_login.data.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+    loginUserPushedInfo();
+}
+
+import { join } from './firstdata.js'
+
+function loginUserPushedInfo() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const newUser = {
+    id: join.users.length,
+    name: "",
+    email: email,
+    phone: "",
+    password: password,
+    login: 1,
+    badge: "./assets/profilebadge/default.svg",
+  };
+  join.users.push(newUser);
+  console.log("user added:", newUser);
+  console.log("All users now:", join.users);
+  window.loginUserPushedInfo = loginUserPushedInfo;
+
+}
+
+window.logIn = function logIn(){
+  const name = document.getElementById("name_sign_up").value;
+  const email = document.getElementById("email_sign_up").value;
+  const password = document.getElementById("password_sign_up").value;
+  const newSignedUser = {
+    id: join.users.length,
+    name: name,
+    email: email,
+    phone: "",
+    password: password,
+    login: 1,
+    badge: "./assets/profilebadge/default.svg",
+  };
+  join.users.push(newSignedUser);
+  console.log(newSignedUser)
+  confirmationSignTemplate();
+}
--- script_login.js
+++ script_login.js
@@ -1,37 +1,45 @@
 let originalLoginHTML;
-
 window.onload = () => {
     originalLoginHTML = document.getElementById("login").innerHTML;
+    loadingScreen();
+    goBack();
+    toggleButton();
 };
 
 function toggleButton() {
     let checkbox = document.getElementById("requiredcheckbox")
     let button = document.getElementById("signUpButton")
     button.disabled = !checkbox.checked;
-    
 }
 
 function goBack() {
-    document.getElementById("login").innerHTML = originalLoginHTML;
+    let signup = document.getElementById("signupPage");
+    let login = document.getElementById("login");
+    signup.classList.add("d_none");
+    login.classList.remove("d_none");
 }
 
-async function renderFunc(){
-    loadingScreen();
-}
+document.addEventListener("DOMContentLoaded", () => {
+  loadingScreen();
+});
 
 async function loadingScreen() {
-    let loadingscreen = document.getElementById("loadingscreen");
+    const loadingscreen = document.getElementById("loadingscreen");
     loadingscreen.innerHTML = loadingScreenDesktopTemplate();
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    document.body.style.display = "block";
-    loadingscreen.innerHTML = "";
-    document.querySelector("header .logo").style.opacity = "1";
+    await new Promise(resolve => setTimeout(resolve, 2500));
+    loadingscreen.style.transition = "opacity 0.5s ease";
+    loadingscreen.style.opacity = "0";
+    await new Promise(resolve => setTimeout(resolve, 500));
+    loadingscreen.remove();
+    document.querySelector(".logo").style.opacity = "1";
 }
 
-function loginUserPushedInfo(){
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-
-    join.push("email", "password");
-    
+function signUp() {
+    let signup = document.getElementById("signupPage");
+    let login = document.getElementById("login");
+    if(signup.classList.contains("d_none")) {
+    login.classList.add("d_none");
+    signup.classList.remove("d_none");
+    }
 }
+
