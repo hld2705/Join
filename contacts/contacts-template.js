1a83600ab3ece88d3fa306ec3098d33ba8539fdc
Halid Crnkic <halidcrnkic@gmail.com>
2025-08-27 20:55:42 +0200

    contact_seite

--- contacts/contacts-template.js
+++ contacts/contacts-template.js
@@ -1,12 +1,29 @@
-import { getUsers } from '/db.js';
+import { join } from '/firstdata.js';
 
-    function contactsLoad() {
-    let contacts = document.getElementById("contactsjs");
-    let users = getUsers();
-    contacts.innerHTML = "";
-    for (let i = 0; i < users.length; i++) {
-      contacts.innerHTML += `<div>${users[i].email}</div>`;
+  function contactsLoad() {
+  let contacts = document.getElementById("contactsjs");
+  let users = join.users.slice().sort((a, b) => a.name.localeCompare(b.name));
+  contacts.innerHTML = "";
+  let currentLetter = "";
+  for (let i = 0; i < users.length; i++) {
+    let firstLetter = users[i].name[0].toUpperCase();
+
+    if (firstLetter !== currentLetter) {
+      currentLetter = firstLetter;
+      contacts.innerHTML += `<h2 class="letter-header">${currentLetter}</h2>
+                            <img src="/assets/Vector 10.svg">`;
     }
+    contacts.innerHTML += `
+      <div class="contact-item">
+
+        <span class="contact-name">
+        <img class="contact-badge" alt="${users[i].name}" src="${users[i].badge}"> <p>${users[i].name}</p>
+        </span>
+
+        <span class="contact-email">${users[i].email}</span>
+      </div>
+    `;
   }
+}
 
   window.contactsLoad = contactsLoad;
