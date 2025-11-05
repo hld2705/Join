a7ded261d72ad99ed1bf153bb94e56f9ecea0554
codeNow26 <code.now26@gmail.com>
2025-11-05 18:30:30 +0100

    board send data to template

--- board/board-templates.js
+++ board/board-templates.js
@@ -2,17 +2,14 @@
 function dragAndDropTemplate(taskId, title, main, description, subtasks) {
 
     let bgColor = "#fff"
-    if(main === "userstory") bgColor = "#0038FF";
-    else if(main === "techtask") bgColor = "#1FD7C1";
-    let displayMain = main;
-    if(main === "userstory") displayMain = "User Story";
-    else if(main === "techtask") displayMain = "Tech Task";
+    if(main === "User Story") bgColor = "#0038FF";
+    else if(main === "Technical Task") bgColor = "#1FD7C1";
 
   return `
     <div class="startendcontainer" ondrop="moveTo(event)" ondragover="dragoverHandler(event)">
       <div id="${taskId}" class="template-wrapper" draggable="true" ondragstart="startDragging(event, ${taskId})">
             <div id="cards" class="board-card">
-                <div class="task-main-container" style="background-color: ${bgColor}">${displayMain}</div> 
+                <div class="task-main-container" style="background-color: ${bgColor}">${main}</div> 
                 <div class="card-container-title-content">
                 <h2>${title}<h2>
                 <p>${description}</p>
