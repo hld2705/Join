1e138f6f2d0e6fd5788b963eada839c840ec7db7
codeNow26 <code.now26@gmail.com>
2025-11-28 16:53:58 +0100

    legal notice, privacy police css, Medium button on board Add Task Overlay is fixed

--- board/board-script.js
+++ board/board-script.js
@@ -267,20 +267,22 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 function openAddTaskOverlay(column) {
-  //console.log(column)
   window.currentTaskColumn = column;
   let overlayBg = document.getElementById("task-overlay-background");
   let overlay = document.getElementById("task-overlay");
-  let container = document.getElementById("task-form-container");
 
   addTaskOverlayTemplate();
+
   if (!addTaskInteractionsLoaded) {
     loadAddTaskInteractions();
-      setTimeout(() => {
-        changeMediumColor();
-    }, 50);
     addTaskInteractionsLoaded = true;
   }
+
+    setTimeout(() => {
+      mediumActive = false;
+      resetAllButton();
+      changeMediumColor();
+    }, 50);
 
   overlayBg.style.display = "block";
   animateOverlayIn(overlay);
