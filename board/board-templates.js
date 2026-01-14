5ebff796099e3652e440aa620f1feaf738aa479b
Halid Crnkić <halidcrnkic@gmail.com>
2026-01-14 11:21:38 +0100

    $

--- board/board-templates.js
+++ board/board-templates.js
@@ -86,26 +86,27 @@ function detailedCardInfoTemplate(task) {
                           if (b.type === "text") {
                             return `
                             <div class="avatar-container">
-                            <div class="badge-name-container">
-                              <div class="avatar-badge"
-                                   style="background-color:${b.badgeColor}; border-color:${b.color}"
-                                   title="${b.name}">
-                                ${b.badge}
-                              </div>
-                              <div>
-                                ${b.name}
-                                 </div>
-                                 </div>
+                                <div class="badge-name-container">
+                                    <div class="avatar-badge"
+                                        style="background-color:${b.badgeColor}; border-color:${b.color}"
+                                            title="${b.name}">
+                                        ${b.badge}
+                                    </div>
+                                    <div>
+                                        ${b.name}
+                                    </div>
+                                </div>
+                            </div>
                             `;
                           } else {
                             return `
-                            <div class="badge-name-container"> <img class="badges-img" src="${b.badge}" title="${b.name}" style="border-color:${b.color}"><p>${b.name}</p> </div>
-                            `;
-                          }
-                        }).join('')}
-                         </div>   
-                    </div>
+                            <div class="badge-name-container">
+                            <img class="badges-img" src="${b.badge}" title="${b.name}" style="border-color:${b.color}">
+                            <p>${b.name}</p> 
+                            </div>
+                            `;}}).join('')}
                     </div>     
+            </div>
             <div class="card-overlay-subtasks-details-container">
                 <p class="label">Subtasks</p>
                 <div class="subtask-render-icons-text">
